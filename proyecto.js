2ff85b449f831fab7b3e1b4941a91adcb67ccd96
hiram98Mtz <hiram98ch@gmail.com>
2024-03-07 09:47:44 -0600

    Texto plano a Html
    Se cambio de texto plano a HTML

--- proyecto.js
+++ proyecto.js
@@ -1,10 +1,10 @@
 const http = require('http')
-const hostname = '192.168.226.229'
+const hostname = '192.168.1.143'
 const port = 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hola Mundo\n');
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hola Mundo</h1>');
 });
 server.listen(port, hostname, () => {
 console.log('El Servidor se está Ejecutando en http: //${hostname}:${port}/');
